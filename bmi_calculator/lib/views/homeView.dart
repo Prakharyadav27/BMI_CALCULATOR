862edea6017a34a0fc9926548b36d9eb36d41ed6
Prakhar <yprakher4@gmail.com>
2024-01-09 23:08:44 +0530

    Refactor main.dart and widget_test.dart

---
+++ bmi_calculator/lib/views/homeView.dart
@@ -0,0 +1,15 @@
+import 'package:bmi_calculator/views/myView.dart';
+import 'package:flutter/material.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      backgroundColor: Colors.white,
+      body: const SafeArea(child: MyView()),
+    );
+  }
+}
